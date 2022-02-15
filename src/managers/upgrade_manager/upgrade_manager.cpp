b430109a4745e6e71a551b3fa7b675b54dc85923
huangchsh <huangchsh@antwork.link>
2022-02-15 19:42:16 +0800

    1.增加更新管理部分函数及注释 2.修改设备界面按钮

--- src/managers/upgrade_manager/upgrade_manager.cpp
+++ src/managers/upgrade_manager/upgrade_manager.cpp
@@ -13,3 +13,42 @@ UpgradeManager::~UpgradeManager()
 {
 
 }
+
+/**
+ * @brief 上传函数
+ * @note 用于上传本地固件或参数
+ * @param[in] file_name: 刷写文件名
+ */
+Q_INVOKABLE void UpgradeManager::upload(const QVariant& file_name)
+{
+
+}
+
+/**
+ * @brief 下载函数
+ * @note 用于下载云端或远端仓库固件或参数
+ * @param[in] file_name: 刷写文件名
+ */
+Q_INVOKABLE void UpgradeManager::download(const QVariant& file_name)
+{
+
+}
+
+/**
+ * @brief 刷写函数
+ * @note 用于选定固件或参数后更新或刷写
+ * @param[in] file_name: 刷写文件名
+ */
+Q_INVOKABLE void UpgradeManager::update(const QVariant& file_name)
+{
+
+}
+
+/**
+ * @brief 出厂配置函数
+ * @note 用于实现固件及参数恢复或初次出厂
+ */
+Q_INVOKABLE void UpgradeManager::factory()
+{
+
+}
--- src/managers/upgrade_manager/upgrade_manager.h
+++ src/managers/upgrade_manager/upgrade_manager.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 
 #include <QObject>
+#include <QVariant>
+#include <QString>
 
 #include "manager_collection.h"
 
@@ -12,10 +14,49 @@ namespace managers
         Q_OBJECT
 
         public:
+            /**
+             * @brief 上传函数
+             * @note 用于上传本地固件或参数
+             * @param[in] file_name: 刷写文件名
+             */
+            Q_INVOKABLE void upload(const QVariant& file_name);
+
+            /**
+             * @brief 下载函数
+             * @note 用于下载云端或远端仓库固件或参数
+             * @param[in] file_name: 刷写文件名
+             */
+            Q_INVOKABLE void download(const QVariant& file_name);
+
+            /**
+             * @brief 刷写函数
+             * @note 用于参数刷写及固件刷写
+             * @param[in] file_name: 刷写文件名
+             */
+            Q_INVOKABLE void flash(const QVariant& file_name);
+
+            /**
+             * @brief 出厂配置函数
+             * @note 用于实现固件及参数恢复或初次出厂
+             */
+            Q_INVOKABLE void factory();
+
+        public:
             UpgradeManager(QApplication *app, ManagerCollection* man_collect);
             ~UpgradeManager();
 
         private:
 
+            /**
+             * @brief 更新函数
+             * @note 用于获取远端更新
+             */
+            void update();
+
+            /**
+             * @brief 获取URL
+             *
+             */
+            void getUrl();
     };
 }   // namespace managers
